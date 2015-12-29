decbfaf43b97dba62660c9523b28ea31946ca2c7
junappleseed <junappleseed@gmail.com>
2015-12-29 22:33:32 +0900

    Initial Commit

---
+++ QiitaClient/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  QiitaClient
+//
+//  Created by Jun on 2015/12/29.
+//  Copyright © 2015年 junappleseed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

a49b94b55436f9dd2d048d1cd6d58e026228ae46
junappleseed <junappleseed@gmail.com>
2016-01-06 23:50:00 +0900

    Gitの管理対象への追加
    Gitの管理対象から外れていたファイルを管理対象に追加しました。

---
+++ QiitaClient/FeedTabViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedTabViewController.swift
+//  QiitaClient
+//
+//  Created by Jun on 2016/01/06.
+//  Copyright © 2016年 junappleseed. All rights reserved.
+//
+
+import UIKit
+
+class FeedTabViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initLabel()
+    }
+    
+    func initLabel(){
+        let size: CGSize = UIScreen.mainScreen().bounds.size
+        let label: UILabel = UILabel(frame: CGRectMake(size.width / 2 - 100, size.height / 2 - 15, 200, 30))
+        
+        label.text = "フィード"
+        label.textAlignment = NSTextAlignment.Center
+        label.textColor = UIColor.whiteColor()
+        label.backgroundColor = UIColor.blueColor()
+        
+        self.view.addSubview(label)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

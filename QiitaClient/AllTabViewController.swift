148dbe2fba9635073cbc0856ffb5841262ebd8e4
junappleseed <junappleseed@gmail.com>
2016-01-11 19:31:18 +0900

    Qiitaの記事取得機能の追加
    AlamofireとSwiftyJSONを使って、Qiitaの記事のタイトルを100件分取得しテーブルに表示する機能を追加しました。

--- QiitaClient/AllTabViewController.swift
+++ QiitaClient/AllTabViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class AllTabViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let items = ["Java", "Ruby", "JavaScript", "C", "Swift", "Lisp"]
+    var articles: [[String: String?]] = []
     var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -28,6 +30,35 @@ class AllTabViewController: UIViewController, UITableViewDelegate, UITableViewDa
         tableView.delegate = self
         
         self.view.addSubview(tableView)
+        
+        self.getArticles()
+    }
+    
+    func getArticles() {
+        let url = "https://qiita.com/api/v2/items"
+        Alamofire.request(.GET, url, parameters: ["per_page": "100"])
+        .responseJSON { response in
+            guard let object = response.result.value else {
+                self.showAlertMessage()
+                return
+            }
+            
+            let json = JSON(object)
+            json.forEach { (_, json) in
+                let article: [String: String?] = [
+                    "title": json["title"].string,
+                ]
+                self.articles.append(article)
+            }
+            self.tableView.reloadData()
+        }
+    }
+    
+    func showAlertMessage() {
+        let alert = UIAlertController(title: "ネットワークエラー", message: "ネットワークに接続できません。", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -35,12 +66,13 @@ class AllTabViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return articles.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
-        cell.textLabel!.text = items[indexPath.row]
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
         return cell
     }
 }
---
+++ QiitaClient/ArticleTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ArticleTableViewCell.swift
+//  QiitaClient
+//
+//  Created by Jun on 2016/01/09.
+//  Copyright © 2016年 junappleseed. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+}
